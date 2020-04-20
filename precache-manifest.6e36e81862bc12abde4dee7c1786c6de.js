66d751f799267a3fe7c0e4cda2d0453a39c49b02
kjenkins5678 <kajenkins9@gmail.com>
2020-04-20 11:57:23 -0400

    deleted readme

---
+++ precache-manifest.6e36e81862bc12abde4dee7c1786c6de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce23d59cc51cb8f4fd9b53e078d5059",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "3522f37a6722387224bb",
+    "url": "/employee-directory/static/css/main.ed059802.chunk.css"
+  },
+  {
+    "revision": "fee9c2686485951fa708",
+    "url": "/employee-directory/static/js/2.0e1b2f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.0e1b2f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3522f37a6722387224bb",
+    "url": "/employee-directory/static/js/main.b56ce4bf.chunk.js"
+  },
+  {
+    "revision": "7725ae6a5b49e8b1096a",
+    "url": "/employee-directory/static/js/runtime-main.3cbf02f2.js"
+  }
+]);
